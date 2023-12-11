2b8d947ef1735721674ad5bd13d1e9bc6741dc02
Simon Heimlicher <simon.github@heimlicher.com>
2023-12-11 23:58:29 +0100

    style: remove custom style for h1 and h2 in mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,17 +2,16 @@
 
 import MarkdownImage from "@/mdx/components/Image";
 import { MDXComponents } from "mdx/types";
-import { PropsWithChildren } from "react";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Use the custom MarkdownImage for image tags
     img: MarkdownImage,
     // Allows customizing built-in components, e.g. to add styling.
-    h1: ({ children }: PropsWithChildren) => (
-      <h1 className="mt-2 text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl md:text-center">{children}</h1>
-    ),
-    h2: ({ children }: PropsWithChildren) => <h2 className="text-zinc-50">{children}</h2>,
+    // h1: ({ children }: PropsWithChildren) => (
+    //   <h1 className="mt-2 text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl md:text-center">{children}</h1>
+    // ),
+    // h2: ({ children }: PropsWithChildren) => <h2 className="text-zinc-50">{children}</h2>,
     ...components,
   };
 }
