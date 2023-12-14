38367d4cefa04c154c1eaeecbe015183154f9f69
Simon Heimlicher <simon.github@heimlicher.com>
2023-12-14 22:02:39 +0100

    style: make account table more compact

--- components/auth/AccountTable.tsx
+++ components/auth/AccountTable.tsx
@@ -14,34 +14,34 @@ export default async function AccountTable({ user, provider, className, ...props
     <Table className={className} {...props}>
       <TableBody className="text-xs">
         <TableRow>
-          <TableHead className="font-medium">Provider</TableHead>
+          <TableHead className="h-auto font-bold">Name</TableHead>
           <TableCell>
-            <span className="font-mono font-bold">{provider}</span>
+            <span className="font-medium">
+              {user?.firstName} {user?.lastName}
+            </span>
           </TableCell>
         </TableRow>
         <TableRow>
-          <TableHead className="font-medium">User ID</TableHead>
+          <TableHead className="h-auto font-medium">Email</TableHead>
+          <TableCell>{user?.email}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableHead className="h-auto font-medium">Provider</TableHead>
+          <TableCell>
+            <span className="font-mono font-medium">{provider}</span>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableHead className="h-auto font-medium">User ID</TableHead>
           <TableCell>
             <AccountId id={user.id} className="text-xs" />.
           </TableCell>
         </TableRow>
         <TableRow>
-          <TableHead className="font-medium">ID</TableHead>
+          <TableHead className="h-auto font-medium">ID</TableHead>
           <TableCell>
             <AccountId id={user?.account.providerAccountId} className="text-xs" />.
           </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableHead className="font-medium">Email</TableHead>
-          <TableCell>{user?.email}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableHead className="font-medium">First name</TableHead>
-          <TableCell>{user?.firstName}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableHead className="font-medium">Last name</TableHead>
-          <TableCell>{user?.lastName}</TableCell>
         </TableRow>
       </TableBody>
     </Table>
